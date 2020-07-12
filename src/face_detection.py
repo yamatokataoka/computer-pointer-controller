530cc79c89de2dc79029ecaedbeaad61cbcfa296
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-07-12 06:33:15 +0000

    Refactor

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,15 +1,10 @@
-'''
-This is a sample class for a model. You may choose to use it as-is or make any changes to it.
-This has been provided just to give you an idea of how to structure your model class.
-'''
-
 class Face_Detection_Model:
     '''
     Class for the Face Detection Model.
     '''
     def __init__(self, model_name, device='CPU', extensions=None):
         '''
-        set your instance variables.
+        set instance variables.
         '''
         self.model_name = model_name
         self.device = device
@@ -56,8 +51,6 @@ class Face_Detection_Model:
         self.input_shape = self.model.inputs[self.input_name].shape
         self.output_name = next(iter(self.model.outputs))
         self.output_shape = self.model.outputs[self.output_name].shape
-
-        return
 
     def predict(self, image):
         '''
