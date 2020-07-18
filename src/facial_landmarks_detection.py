8e002b41861934e15fdde4a52d4b3e91ec5288b8
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-07-18 05:06:03 +0000

    Implement generic class for each model

--- src/facial_landmarks_detection.py
+++ src/facial_landmarks_detection.py
@@ -1,51 +1,17 @@
-import os
-import sys
-import logging as log
-import cv2
-from openvino.inference_engine import IENetwork, IECore
+from model import Model
 
-class Facial_Landmarks_Detection:
+class Facial_Landmarks_Detection(Model):
     '''
     Class for the Facial Landmarks Detection Model.
     '''
-    def __init__(self, model_name, device='CPU', extensions=None, threshold=0.6):
-        '''
-        set instance variables.
-        '''
-        self.model_name = model_name
-        self.device = device
-        self.extensions = extensions
-        self.plugin = None
-        self.network = None
-        self.exec_network = None
-        self.input_name = None
-        self.input_shape = None
-        self.output_name = None
-        self.output_shape = None
-        self.threshold=threshold
+    def __init__(self, model_name, device='CPU', extensions=None):
+        super(Facial_Landmarks_Detection, self).__init__(model_name, device, extensions)
 
-    def load_model(self):
-        ### Load the Inference Engine API
-        self.plugin = IECore()
-
-        ### Load the model
-        model_xml = self.model_name
-        model_bin = os.path.splitext(model_xml)[0] + ".bin"
-        self.network = IENetwork(model=model_xml, weights=model_bin)
-
-        ### Check model extesions and any unsupported layers
-        self.check_model()
-
-        ### Load the model network into a self.plugin variable
-        self.exec_network = self.plugin.load_network(self.network, self.device)
-
-        log.info("IR successfully loaded into Inference Engine.")
-
-        ### Get the input and output information
         self.input_name = next(iter(self.network.inputs))
         self.input_shape = self.network.inputs[self.input_name].shape
+        self.input_width = self.input_shape[2]
+        self.input_hight = self.input_shape[3]
         self.output_name = next(iter(self.network.outputs))
-        self.output_shape = self.network.outputs[self.output_name].shape
 
     def predict(self, image):
         '''
@@ -61,35 +27,8 @@ class Facial_Landmarks_Detection:
 
         return outputs
 
-    def check_model(self):
-        '''
-        Check adding extensions and any unsupported layers
-        '''
-        ### Add a CPU extension, if applicable
-        if self.extensions and "CPU" in self.device:
-            self.plugin.add_extension(self.extensions, self.device)
-
-        ### Check for supported layers
-        supported_layers = self.plugin.query_network(network=self.network, device_name=self.device)
-
-        ### Check for any unsupported layers, and let the user
-        ### know if anything is missing. Exit the program, if so.
-        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
-        if len(unsupported_layers) != 0:
-            log.warn("Unsupported layers found: {}".format(unsupported_layers))
-            log.warn("Check whether extensions are available to add to IECore.")
-            exit(1)
-
     def preprocess_input(self, image):
-        '''
-        Before feeding the data into the model for inference,
-        preprocess image.
-        '''
-        p_frame = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
-        p_frame = p_frame.transpose((2,0,1))
-        p_frame = p_frame.reshape(1, *p_frame.shape)
-
-        return p_frame
+        super().preprocess_input(image, self.input_hight, self.input_width)
 
     def preprocess_output(self, outputs):
         '''
--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -1,48 +1,15 @@
-import os
-import sys
-import logging as log
 import numpy as np
-import math
-from openvino.inference_engine import IENetwork, IECore
+import logging as log
+from model import Model
 
-class Gaze_Estimation:
+class Gaze_Estimation(Model):
     '''
     Class for the Gaze Estimation Model.
     '''
-    def __init__(self, model_name, device='CPU', extensions=None, threshold=0.6):
-        '''
-        set instance variables.
-        '''
-        self.model_name = model_name
-        self.device = device
-        self.extensions = extensions
-        self.plugin = None
-        self.network = None
-        self.exec_network = None
-        self.input_shape = None
-        self.output_name = None
-        self.threshold=threshold
 
-    def load_model(self):
-        ### Load the Inference Engine API
-        self.plugin = IECore()
+    def __init__(self, model_name, device='CPU', extensions=None):
+        super(Gaze_Estimation, self).__init__(model_name, device, extensions)
 
-        ### Load the model
-        model_xml = self.model_name
-        model_bin = os.path.splitext(model_xml)[0] + ".bin"
-        self.network = IENetwork(model=model_xml, weights=model_bin)
-
-        ### Check model extesions and any unsupported layers
-        self.check_model()
-
-        ### Load the model network into a self.plugin variable
-        self.exec_network = self.plugin.load_network(self.network, self.device)
-
-        log.info("IR successfully loaded into Inference Engine.")
-
-        ### Get the input and output information
-        self.input_name = next(iter(self.network.inputs))
-        self.input_shape = self.network.inputs[self.input_name].shape
         self.output_name = next(iter(self.network.outputs))
 
     def predict(self, eyes, head_pose_angles):
@@ -58,25 +25,6 @@ class Gaze_Estimation:
         outputs = self.preprocess_output(outputs)
 
         return outputs
-
-    def check_model(self):
-        '''
-        Check adding extensions and any unsupported layers
-        '''
-        ### Add a CPU extension, if applicable
-        if self.extensions and "CPU" in self.device:
-            self.plugin.add_extension(self.extensions, self.device)
-
-        ### Check for supported layers
-        supported_layers = self.plugin.query_network(network=self.network, device_name=self.device)
-
-        ### Check for any unsupported layers, and let the user
-        ### know if anything is missing. Exit the program, if so.
-        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
-        if len(unsupported_layers) != 0:
-            log.warn("Unsupported layers found: {}".format(unsupported_layers))
-            log.warn("Check whether extensions are available to add to IECore.")
-            exit(1)
 
     def preprocess_output(self, outputs):
         '''
