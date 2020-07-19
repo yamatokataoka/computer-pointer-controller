30879d056734dc78c31f90f203db4b3f590f023d
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-07-19 01:01:18 +0000

    Implement preprocessing on Gaze_Estimation class

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import logging as log
 from model import Model
@@ -10,12 +11,18 @@ class Gaze_Estimation(Model):
     def __init__(self, model_name, device='CPU', extensions=None):
         super(Gaze_Estimation, self).__init__(model_name, device, extensions)
 
+        self.input_shape = self.network.inputs['left_eye_image'].shape
+        self.input_width = self.input_shape[2]
+        self.input_height = self.input_shape[3]
         self.output_name = next(iter(self.network.outputs))
 
     def predict(self, eyes, head_pose_angles):
         '''
         This method is meant for running predictions on the input image.
         '''
+        for i in range(2):
+            eyes[i] = self.preprocess_input(eyes[i])
+
         input_dict={'left_eye_image'  : eyes[0],
                     'right_eye_image' : eyes[1],
                     'head_pose_angles': head_pose_angles}
@@ -25,6 +32,10 @@ class Gaze_Estimation(Model):
         outputs = self.preprocess_output(outputs)
 
         return outputs
+
+    def preprocess_input(self, image):
+        processed_image = super().preprocess_input(image, self.input_height, self.input_width)
+        return processed_image
 
     def preprocess_output(self, outputs):
         '''
